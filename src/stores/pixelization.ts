e2c6bffec3b857deb71d49fabd3d9926be83d647
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2026-08-30 02:49:33 +0800

    [Update] Add NES palette option. Add data ditherStrength.

---
+++ src/stores/pixelization.ts
@@ -0,0 +1,48 @@
+import { defineStore } from 'pinia'
+
+export type Palette = {
+  id: string
+  name: string
+  colours: string[]
+}
+
+export const usePixelizationStore = defineStore('pixelization', {
+  state: () => ({
+    pixelSizes: [2, 4, 6, 8],
+    palettes: [
+      {
+        id: 'original',
+        name: 'Original colour',
+        colours: ['#fb7185', '#fbbf24', '#38bdf8', '#9d5cff'],
+      },
+      {
+        id: 'gameboy',
+        name: 'Game Boy',
+        colours: ['#0f380f', '#306230', '#8bac0f', '#9bbc0f'],
+      },
+      {
+        id: 'nes',
+        name: 'NES',
+        colours: ['#000000', '#0078f8', '#a80020', '#007800'],
+      }
+    ] as Palette[],
+    selectedSize: 4,
+    selectedPalette: 'original',
+    ditherStrength: 0.35
+  }),
+  getters: {
+    activePalette: (state) =>
+      state.palettes.find((palette) => palette.id === state.selectedPalette) ?? state.palettes[0]!,
+  },
+  actions: {
+    setPixelSize(size: number) {
+      this.selectedSize = size
+    },
+    setPalette(palette: string) {
+      this.selectedPalette = palette
+    },
+    setDitherStrength(strength: number) {
+      this.ditherStrength = strength
+    }
+  },
+})
