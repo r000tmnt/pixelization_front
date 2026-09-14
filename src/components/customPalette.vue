7b788dc1fe0dd24c01084a80be77adbc315bf474
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2026-09-14 15:48:37 +0800

    [Update] Change wrapper position based on scrollTop. Adjust slider. RWD.

--- src/components/customPalette.vue
+++ src/components/customPalette.vue
@@ -1,21 +1,23 @@
 <template>
   <Transition name="fade">
-    <div v-if="display" class="wrapper">
+    <div v-if="display" class="wrapper"
+      :style="{ marginTop: `${scrollTop}px` }">
       <div class="content">
         <div class="palette-header">
           <h3>Custom Palette</h3>
           <button class="close secondary" @click="emit('close')">X</button>
         </div>
 
-        <div>
+        <div class="slider">
           <label>Range of color</label>
           <input
             type="range"
             :min="2"
             :max="16"
-            :step="2"
+            :step="1"
             :value="colors.length"
             @change="updateColorSlots" />
+          <div>{{ colors.length }}</div>
         </div>
 
         <div class="color-grid">
@@ -51,9 +53,16 @@
 <script setup lang="ts">
   import { storeToRefs } from 'pinia'
   import { usePixelizationStore } from '../stores/pixelization'
-  import { ref, onMounted, computed } from 'vue'
+  import { useDefaultStore } from '@/stores/default';
+  import { ref, onMounted, onUnmounted, computed } from 'vue'
 
   const settings = usePixelizationStore()
+
+  const defaultStore = useDefaultStore()
+
+  const { scrollTop } = storeToRefs(defaultStore)
+
+  // const { setScrollTop } = defaultStore
 
   defineProps<{
     display: boolean
@@ -99,10 +108,24 @@
     }
   }
 
+  // const ifScrollTop = () => {
+  //   if(window.innerWidth >= 899 && scrollTop.value > 0){
+  //     setScrollTop(0)
+  //   }
+  // }
+
   onMounted(() => {
     if(selectedColors.value.length){
       colors.value = selectedColors.value.map(color => color)
     }
+
+    // addEventListener('resize', ifScrollTop)
+
+    document.body.style.overflow = 'hidden'
+  })
+
+  onUnmounted(() => {
+    document.body.style.overflow = 'unset'
   })
 </script>
 
@@ -120,12 +143,14 @@
     left: 50%;
     transform: translate(-50%, -50%);
     width: 30%;
-    height: 30%;
+    min-width: 520px;
+    /* height: 30%; */
     background: var(--px-surface);
     border-radius: 4px;
     padding: 1% 2%;
     display: flex;
     flex-direction: column;
+    transition: all 0.3s ease;
   }
 
   .palette-header{
@@ -137,6 +162,7 @@
 
   .close{
     cursor: pointer;
+    border-radius: 8px;
   }
 
   .color-grid{
@@ -148,7 +174,7 @@
   }
 
   .color-box {
-    width: 50px;
+    /* width: 50px; */
     height: 50px;
     border: 2px solid #ccc;
     border-radius: 8px;
@@ -164,5 +190,16 @@
   .save{
     width: 100%;
     margin-top: auto;
+  }
+
+  @media (max-width: 576px) {
+    .content{
+      min-width: 80%;
+      padding: 2% 5%;
+    }
+
+    .color-grid{
+      grid-template-columns: 1fr 1fr 1fr 1fr;
+    }
   }
 </style>
