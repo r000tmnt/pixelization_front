5241d22059cb25e9db6b2e47d7a718ab901128ee
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2026-09-14 21:08:49 +0800

    [Create] Footer component

---
+++ src/components/footerSection.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer>
+    <div>©2026 ParkCorner All Rights Reserved.</div>
+  </footer>
+</template>
+
+<!-- <script setup></script> -->
+
+<style scoped>
+  footer {
+    color: var(--px-text-muted);
+    font-size: 12px;
+    text-align: center;
+    padding: 30px 0;
+  }
+</style>
