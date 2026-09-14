e6cb508eed131592a1ee320ede074ecdf12be811
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2026-09-14 15:47:31 +0800

    [Update] Assign scroll event listener.

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,19 @@
 <template>
   <router-view />
 </template>
+
+<script lang="ts">
+  import { useDefaultStore } from './stores/default';
+
+  // const defaultStore = useDefaultStore()
+
+  const onScroll = () => {
+    console.log('scroll', window.scrollY)
+
+    const { setScrollTop } = useDefaultStore()
+
+    setScrollTop(window.scrollY)
+  }
+
+  addEventListener('scroll', onScroll)
+</script>
