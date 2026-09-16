50d8d3da6e02b0f6e0da40f132afd557f98e1ae1
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2026-09-16 23:38:18 +0800

    [WIP] Page for 404.

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,6 +8,11 @@ const router = createRouter({
       name: 'Home',
       component: () => import('../views/HomeView.vue'),
     },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: () => import('../views/notFound.vue'),
+    }
   ],
 })
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,28 @@
+<template>
+  <p>
+    404 NOT FOUND
+  </p>
+  <div>
+    Return to home page after {{ sec }} seconds.
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const counter = ref<number>(0)
+const sec = ref<number>(5)
+
+onMounted(() => {
+  counter.value = setInterval(() => {
+    if(sec.value === 0){
+      clearInterval(counter.value)
+      router.push('/')
+    }
+    sec.value = sec.value - 1
+  }, 1000)
+})
+</script>
