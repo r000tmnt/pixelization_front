886778ccbae9349d37e4eea891cd61703dcada6b
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2026-09-16 11:34:29 +0800

    [WIP] Adjust style.

--- src/components/localeDialog.vue
+++ src/components/localeDialog.vue
@@ -1,12 +1,19 @@
 <template>
-  <section class="wrapper">
+  <section class="wrapper" @click="emit('close')">
     <div class="content">
+      <div class="dialog-header">
+        <h3>Language</h3>
+        <!-- <button class="close secondary" @click="emit('close')">X</button> -->
+      </div>
+
+      <div class="locale-options">
         <button v-for="id in list" :key="id"
           class="button"
           :class="`${lang === id? 'primary' : 'secondary'}`"
-          @click="setLocale(id)">
-          {{ id.toUpperCase() }}
+          @click.stop="setLocale(id)">
+          {{ setDisplayLang(id) }}
         </button>
+      </div>
     </div>
   </section>
 </template>
@@ -18,7 +25,9 @@ import { storeToRefs } from 'pinia';
 
 const defaultStore = useDefaultStore()
 const { lang } = storeToRefs(defaultStore)
-const { setLocale } = defaultStore
+const { setLocale, setDisplayLang } = defaultStore
+
+const emit = defineEmits<{'close': []}>()
 </script>
 
 <style scoped>
@@ -29,17 +38,25 @@ const { setLocale } = defaultStore
     background: rgba(0, 0, 0, 0.75);
   }
 
+  .dialog-header {
+    justify-content: center;
+  }
+
   .content {
     position: relative;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    width: 30%;
-    min-width: 520px;
+    width: 200px;
+    /* min-width: 520px; */
     /* height: 30%; */
     background: var(--px-surface);
-    border-radius: 4px;
-    padding: 23px 2% 23px 2%;
+    border-radius: 8px;
+    padding: 23px;
+    padding-top: 1px
+  }
+
+  .locale-options{
     display: flex;
     flex-direction: column;
     transition: all 0.3s ease;
