9e648e8ab063eef7387fe7b7da15039c2f6fb0e1
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2026-09-16 17:09:05 +0800

    [Update] Use unHead.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createHead } from '@unhead/vue/client'
 import { i18n } from './locale/export.ts'
 
 import './app.css'
@@ -8,9 +9,11 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const head = createHead()
 
 app.use(createPinia())
 app.use(router)
 app.use(i18n)
+app.use(head)
 
 app.mount('#app')
