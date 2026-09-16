b97a2f9fa1dedda8660b1f53e66ff0b01ced80ef
r000tmnt <rick.t.ryahoocomtw653@gmail.com>
2026-09-16 13:58:44 +0800

    [Update] Use the first 4 of chosen color for custom option.

--- src/stores/pixelization.ts
+++ src/stores/pixelization.ts
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia'
 export type Palette = {
   id: string
   name: string
-  colours: string[]
+  colors: string[]
 }
 
 export const usePixelizationStore = defineStore('pixelization', {
@@ -13,37 +13,38 @@ export const usePixelizationStore = defineStore('pixelization', {
       {
         id: 'original',
         name: 'Original',
-        colours: ['#fb7185', '#fbbf24', '#38bdf8', '#9d5cff'],
+        colors: ['#fb7185', '#fbbf24', '#38bdf8', '#9d5cff'],
       },
       {
         id: 'gameboy',
         name: 'Game Boy',
-        colours: ['#0f380f', '#306230', '#8bac0f', '#9bbc0f'],
+        colors: ['#0f380f', '#306230', '#8bac0f', '#9bbc0f'],
       },
       {
         id: 'nes',
         name: 'NES',
-        colours: ['#000000', '#0078f8', '#a80020', '#007800'],
+        colors: ['#000000', '#0078f8', '#a80020', '#007800'],
       },
       {
         id: 'snes',
         name: 'SNES',
-        colours: ['#2d5280', '#732866', '#f27961', '#b3b324'],
+        colors: ['#2d5280', '#732866', '#f27961', '#b3b324'],
       },
       {
         id: '1bit',
         name: '1BIT',
-        colours: ['#000000', '#ffffff', '#f2796100', '#b3b32400'],
+        colors: ['#000000', '#ffffff', '#f2796100', '#b3b32400'],
       },
       {
         id: 'monochrome',
         name: 'Monochrome',
-        colours: ['#000000', '#222222', '#444444', '#666666'],
+        colors: ['#000000', '#222222', '#444444', '#666666'],
       },
       {
         id: 'custom',
         name: 'Custom',
-        colours: ['#f2796100', '#b3b32400', '#f2796100', '#b3b32400'],
+        //TODO:  Use the first 4 of chosen color
+        colors: ['#f2796100', '#b3b32400', '#f2796100', '#b3b32400'],
       }
     ] as Palette[],
     acceptFileTypes: ['image/png', 'image/jpeg', 'image/webp'],
@@ -87,6 +88,18 @@ export const usePixelizationStore = defineStore('pixelization', {
     },
     setCustomColors(colors: string[]) {
       this.selectedColors = colors
+
+      // console.log(colors)
+
+      const customIndex = this.palettes.length - 1
+      const customPalette = this.palettes[customIndex]
+
+      if (!customPalette) return
+
+      for (let i = 0; i < 4; i++) {
+        const color = colors[i] ?? customPalette.colors[i] ?? '#00000000'
+        customPalette.colors[i] = color
+      }
     }
   },
 })
